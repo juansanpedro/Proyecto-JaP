c1be39b77697e580fdccf61c22aa059e9e2214c8
juansanpedro <juanignacio50@hotmail.com>
2022-08-19 01:37:08 -0300

    .

--- js/products.js
+++ js/products.js
@@ -1,8 +1,21 @@
-const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const container = document.getElementById("container")
 
-.then(url)
-.then((respuesta)=>respuesta.json())
-.then((products)=>{
-    console.log(products)
-});
+function products(array){
+for (const auto of array) {
+    container.innerHTML += `<div onclick="setCatID(${auto.id})" class="list-group-item list-group-item-action cursor-active">
+    <div class="row">
+        <div class="col-3">
+            <img src="${auto.image}" alt="${auto.description}" class="img-thumbnail">
+        </div>
+        <div class="col">
+            <div class="d-flex w-100 justify-content-between">
+                <h4 class="mb-1">${auto.name}</h4>
+                <small class="text-muted">${auto.productCount} artículos</small>
+            </div>
+            <p class="mb-1">${auto.description}</p>
+        </div>
+    </div>
+</div>`
+}
+}
