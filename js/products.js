129267c442396444be7407f398b89f3c5279bcb2
juansanpedro <juanignacio50@hotmail.com>
2022-08-19 01:22:30 -0300

    .

--- js/products.js
+++ js/products.js
@@ -1,3 +1,8 @@
 const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const container = document.getElementById("container")
 
+.then(url)
+.then((respuesta)=>respuesta.json())
+.then((products)=>{
+    console.log(products)
+});
