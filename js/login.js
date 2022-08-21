5a8f4a7171fb444144eaf9eed75f855ca8b3a1f0
juansanpedro <juanignacio50@hotmail.com>
2022-08-21 19:17:17 -0300

    .

--- js/login.js
+++ js/login.js
@@ -1,11 +1,20 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
 function validar(){
     let email = document.getElementById("email");
     let contraseña = document.getElementById("contraseña");
 
     if((email.value.length ===0 )|| (contraseña.value.length ===0)){
-        console.log("Algunos de los campos estan vacios");
+        showAlertError();
     }else{
-        location.href = "http://127.0.0.1:5500/index.html";
+        location.href = ("http://127.0.0.1:5500/index.html");
+        showAlertSuccess();
     }
 }
 
