510421a6a00347f390313424e397d936febea85c
juansanpedro <juanignacio50@hotmail.com>
2022-08-27 22:50:13 -0300

    .

--- js/products.js
+++ js/products.js
@@ -33,30 +33,3 @@ fetch(URL)
 
 .then(data =>{productos(data.products)}) 
 
-/*
-
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
-    });
-}
-*/
