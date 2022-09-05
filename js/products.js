037b7018769d1dbe15e3c3ab617e1509b5d6443a
juansanpedro <juanignacio50@hotmail.com>
2022-09-04 22:33:14 -0300

    .

--- js/products.js
+++ js/products.js
@@ -1,35 +1,20 @@
 let acceso = localStorage.getItem("catID");
-const URL_JS = `${PRODUCTS_URL}${acceso}${EXT_TYPE}`; // realizar peticion 
+const URL = `${PRODUCTS_URL}${acceso}${EXT_TYPE}`; // realizar peticion
 const container = document.getElementById("container");
-const ascendente = document.getElementById("asc");
-const descendente = document.getElementById("desc");
-const relevancia = document.getElementById("rel");
-const ordenando = document.getElementById("ordenando");
-let array = [];   
+let sortAsc1 = document.getElementById("sortAsc1");
+let sortDesc2 = document.getElementById("sortDesc2");
+let sortByCount3 = document.getElementById("sortByCount3");
 
-function sub(data){
-    let sub = document.getElementById("sub");
-    sub.innerHTML = `<p>Veras aqui todos los productos de la categoria ${data.catName}</p>` 
-}
+let limpiar = document.getElementById("limpiar");
+let filtrar = document.getElementById("filtrar");
+let max = document.getElementById("max");
+let min = document.getElementById("min");
+let array = [];
 
-fetch(URL_JS)
-.then(res=>{
-    if (res.ok) {
- return res.json()
-    }else{
-    console.log("Error")
-    }
-})
-.then(data =>{ 
-    array = data.products;
-    productos();
-    sub(data);
-    
-}) 
-
-function productos(){
-for (const element of array) {
-    container.innerHTML += `<div onclick="setCatID(${element.id})" class="list-group-item list-group-item-action cursor-active">
+function productos() {
+  let htmlContentToAppend = "";
+  for (const element of array) {
+    htmlContentToAppend += `<div onclick="setCatID(${element.id})" class="list-group-item list-group-item-action cursor-active">
     <div class="row">
         <div class="col-3">
             <img src="${element.image}" alt="${element.description}" class="img-thumbnail">
@@ -42,17 +27,98 @@ for (const element of array) {
             <p class="mb-1">${element.description}</p>
         </div>
     </div>
-</div>`
-}
+</div>`;
+container.innerHTML = htmlContentToAppend;
+  }
 }
 
- let prueba = document.getElementById("prueba");
- prueba.addEventListener("click",function(){
-    console.log("funciona");
-  array.sort((a, b) => {
-      if (a.cost < b.cost) {return 1;}
-      if (a.cost > b.cost) {return -1;}
-      return 0;
-    });
+function sub(data) {
+  let sub = document.getElementById("sub");
+  sub.innerHTML = `<p>Veras aqui todos los productos de la categoria ${data.catName}</p>`;
+}
+
+fetch(URL)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      console.log("Error");
+    }
+  })
+  .then((data) => {
+    sub(data);
+    array = data.products;
     productos();
- });
+  });
+
+sortAsc1.addEventListener("click", function () {
+  array.sort((a, b) => {
+    if (a.cost < b.cost) {
+      return -1;
+    }
+    if (a.cost > b.cost) {
+      return 1;
+    }
+    return 0;
+  });
+  productos();
+});
+
+sortDesc2.addEventListener("click", function () {
+  array.sort((a, b) => {
+    if (a.cost < b.cost) {
+      return 1;
+    }
+    if (a.cost > b.cost) {
+      return -1;
+    }
+    return 0;
+  });
+  productos();
+});
+
+sortByCount3.addEventListener("click", function () {
+  array.sort((a, b) => {
+    if (a.soldCount > b.soldCount) {
+      return -1;
+    }
+    if (a.soldCount < b.soldCount) {
+      return 1;
+    }
+    return 0;
+  });
+  productos();
+});
+
+filtrar.addEventListener("click" , function(){ 
+let min1 = min.value;
+let max1 = max.value;
+let min3 = undefined;
+let max3 = undefined;
+
+if ((min1 != undefined) && (min1 != "") && (parseInt(min1)) >= 0){
+    min3 = parseInt(min1);
+}
+else{
+    min3 = undefined;
+}
+
+if ((max1 != undefined) && (max1 != "") && (parseInt(max1)) >= 0){
+    mix3 = parseInt(max1);
+}
+else{
+    mix3 = undefined;
+}
+array = array.filter((value)=> (value.cost > min3 && value.cost < max3));
+productos();
+});
+
+limpiar.addEventListener("click", function(){
+    min.value = "";
+    max.value = "";
+
+    min = undefined;
+    max = undefined;
+
+    productos();
+});
