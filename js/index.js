cb2cb817b2993e6e8e75c1ffb7c93ea36760f9d1
juansanpedro <juanignacio50@hotmail.com>
2022-09-28 22:36:46 -0300

    .

--- js/index.js
+++ js/index.js
@@ -15,7 +15,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let conexion = localStorage.getItem("usuario")
 let parrafo = document.getElementById("inicio");
-parrafo.innerHTML = `<li>${conexion}</li>`
+parrafo.innerHTML = `<div class="btn-group">
+<button type="button" class="btn btn-warning">${conexion} </button>
+<button type="button" class="btn btn-warning dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+  <span class="visually-hidden">Toggle Dropdown</span>
+</button>
+<ul class="dropdown-menu">
+  <li><a class="dropdown-item" href="cart.html">Mi Carrito</a></li>
+  <li><a class="dropdown-item" href="my-profile.html">Mi Perfil</a></li>
+  <li><a class="dropdown-item" href="index.html">Cerrar Sesion</a></li>
+</ul>
+</div>`
 
 
 
