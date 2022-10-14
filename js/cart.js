4a8b82f2ef5acd196f09170397a19652154f9dd6
juansanpedro <juanignacio50@hotmail.com>
2022-10-14 00:51:46 -0300

    .

--- js/cart.js
+++ js/cart.js
@@ -1,27 +1,40 @@
-const usuarioId = 25801;
+const usuarioCar = 25801;
 const contenido = document.getElementById("car");
 
 document.addEventListener("DOMContentLoaded", function () {
-  let URL_CART = CART_INFO_URL + usuarioId + EXT_TYPE;
-  fetch(URL_CART)
+  let URL_cart = CART_INFO_URL + usuarioCar + EXT_TYPE;
+  fetch(URL_cart)
     .then(function (response) {
       return response.json();
     })
     .then(function (item) {
-      hola(item);
+      Carrito(item);
     });
 });
 
-function hola(data) {
+function Carrito(data) {
   let htmlContentToAppend = "";
   for (const item of data.articles) {
-    htmlContentToAppend += `<div (${item.id})" class="col-md-4">
-    <div class="card mb-4 list-group-item-action cursor-active">
-    <img class="bd-placeholder-img card-img-top" src="${cart.image}" class="img-thumbnail">
-    <p class="m-3">${item.name}</p>
-     </div>
-    </div>`
+    htmlContentToAppend += `<table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Costo</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">SubTotal</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><img src="${item.image}"></td>
+        <td>${item.name}</td>
+        <td>${item.currency} ${item.unitCost}</td>
+        <td> <input class="number whi"></td>
+        <td><strong>${item.currency} ${item.unitCost}</strong></td>
+      </tr>`
     ;
     contenido.innerHTML = htmlContentToAppend;
   }
 }
+
