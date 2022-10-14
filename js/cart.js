a2fe3883efe6655666957d6623703ae86a665366
juansanpedro <juanignacio50@hotmail.com>
2022-10-13 23:04:48 -0300

    peticion

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 const usuarioId = 25801;
-contenedor = document.getElementById("carrito");
+const contenido = document.getElementById("car");
 
 document.addEventListener("DOMContentLoaded", function () {
   let URL_CART = CART_INFO_URL + usuarioId + EXT_TYPE;
@@ -8,20 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
       return response.json();
     })
     .then(function (item) {
-      Carrito(item);
+      hola(item);
     });
 });
 
-function Carrito(data) {
+function hola(data) {
   let htmlContentToAppend = "";
-  for (const cart of data.articles) {
-    htmlContentToAppend += `<div (${cart.id})" class="col-md-4">
+  for (const item of data.articles) {
+    htmlContentToAppend += `<div (${item.id})" class="col-md-4">
     <div class="card mb-4 list-group-item-action cursor-active">
     <img class="bd-placeholder-img card-img-top" src="${cart.image}" class="img-thumbnail">
-    <p class="m-3">${cart.name}</p>
+    <p class="m-3">${item.name}</p>
      </div>
     </div>`
     ;
-    contenedor.innerHTML = htmlContentToAppend;
+    contenido.innerHTML = htmlContentToAppend;
   }
 }
