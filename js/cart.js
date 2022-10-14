27cf9f19c39504d7a0f6bbde2467fb55e2838d6f
juansanpedro <juanignacio50@hotmail.com>
2022-10-13 22:46:51 -0300

    .

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,26 @@
-let cart = localStorage.getItem("user");
-const URL_CART = "https://japceibal.github.io/emercado-api/user_cart/" + cart + EXT_TYPE;
-carrito = document.getElementById("carrito");
+const usuarioId = 25801;
+contenedor = document.getElementById("carrito");
 
-
-function carr() {
-    let htmlContentToAppend = "";
-    for (const cart of lista.articles) {
-      htmlContentToAppend += `<div> Hola ${cart.name} car</div>`;
-      carrito.innerHTML = htmlContentToAppend;
-    }
-  }   
-
-  document.addEventListener("DOMContentLoaded", function () {
-    getJSONData(URL_CART).then(function (resultObj) {
-      if (resultObj.status === "ok") {
-        lista = resultObj.data;
-        carr(lista); 
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  let URL_CART = CART_INFO_URL + usuarioId + EXT_TYPE;
+  fetch(URL_CART)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (item) {
+      Carrito(item);
     });
-  });
-  
+});
+
+function Carrito(data) {
+  let htmlContentToAppend = "";
+  for (const item of data.articles) {
+    htmlContentToAppend += `<div onclick="cargarRelacionados(${item.id})" class="col-md-4">
+    <div class="card mb-4 list-group-item-action cursor-active">
+    <img class="bd-placeholder-img card-img-top" src="${item.image}" class="img-thumbnail">
+    <p class="m-3">${item.name}</p>
+     </div>
+    </div>`;
+    contenedor.innerHTML = htmlContentToAppend;
+  }
+}
