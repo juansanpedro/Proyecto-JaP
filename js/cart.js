32d8b35f50a08cb8e02ba341cfff8535edcdc259
juansanpedro <juanignacio50@hotmail.com>
2022-10-16 20:51:56 -0300

    oniimput

--- js/cart.js
+++ js/cart.js
@@ -1,47 +1,46 @@
-const URL_CART = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
-var shopping = document.getElementById("car");
+const URL_CART =
+  "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+let contenido = document.getElementById("car");
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(URL_CART).then(function(resultObj){
-        if (resultObj.status=='ok'){
-            CartObj = resultObj.data;
-            ShowCart(CartObj.articles);
-        }
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  getJSONData(URL_CART).then(function (resultObj) {
+    if (resultObj.status == "ok") {
+      CartObj = resultObj.data;
+      Carrito(CartObj.articles);
+    }
+  });
 });
-function ShowCart(array) {
-    let htmlContentToAppend = "";
-    for(let carts of array){
-    htmlContentToAppend += `<div class="container title">
-            <table>
+function Carrito(array) {
+  let htmlContentToAppend = "";
+  for (let carts of array) {
+    htmlContentToAppend += `<table class="table table-dark"> <br>
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Costo</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">SubTotal</th>
+      </tr>
+    </thead>
+    <tbody>
                 <tr>
-                    <td class="Distancia"><b>Producto</b></td>
-                    <td class="Distancia"><b>Nombre</b></td>
-                    <td class="Distancia"><b>Costo</b></td>
-                    <td class="Distancia"><b>Cantidad</b></td>
-                    <td class="Distancia"><b>Subtotal</b></td>
-                </tr>
-                <tr>
-                    <div class="DivBuyProducto">
-                    <td class="Distancia"><img src="${carts.image}" class="ImgCart"></td>
-                    <td class="Distancia">${carts.name}</td>
-                    <td class="Distancia">${carts.currency}${carts.unitCost}</td>
-                    <td class="Distancia">
-                        <input id="ctd" oninput="subTotal(${carts.unitCost})" type="number" class="InputCtd" >
-                    </td>
-                    <td class="Distancia"><b><p>${carts.currency}<span id="resultado">${carts.unitCost}</p></b></td>
+                    <td><img src="${carts.image}" style="width: 65px; height: 40px;"  class="img-fluid"</td>
+                    <td>${carts.name}</td>
+                    <td>${carts.currency}${carts.unitCost}</td>
+                    <td><input id="cant" oninput="subTotal(${carts.unitCost})"type="number" class="InputCtd"></td>
+                    <td><b><p>${carts.currency}<span id="resultado">${carts.unitCost}</p></b></td>
                     <div>
                 </tr>
             </table>
     </div>
-    `}
-        shopping.innerHTML = htmlContentToAppend;
+    `;
+  }
+  contenido.innerHTML = htmlContentToAppend;
 }
 
-
-function subTotal(precio){
-    let ctd = document.getElementById("ctd").value 
-    resultado = precio*ctd
-    return document.getElementById("resultado").innerHTML = resultado
-} 
+function subTotal(precio) {
+  let ctd = document.getElementById("cant").value;
+  resultado = precio * ctd;
+  return (document.getElementById("resultado").innerHTML = resultado);
+}
