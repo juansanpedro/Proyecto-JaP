eaa0287e4af30a7b1988894c00ac4f1c59fd7fa4
juansanpedro <juanignacio50@hotmail.com>
2022-10-28 23:26:34 -0300

    .

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,8 @@ const URL_CART =
   "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 let contenido = document.getElementById("car");
 
+let costos = document.getElementById("costos"); // Entrega 6
+
 document.addEventListener("DOMContentLoaded", function () {
   getJSONData(URL_CART).then(function (resultObj) {
     if (resultObj.status == "ok") {
@@ -34,7 +36,30 @@ function Carrito(array) {
                     <td><b>${carts.currency}<span id="resultado">${carts.unitCost}</b></td>
                 </tr>
             </table>
-    `;
+
+    `
+     costos.innerHTML += ` 
+   <ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Subtotal del producto (U$D)
+    <span class="badge bg-primary rounded-pill" id="costoss">${carts.unitCost}</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Costo del envio (U$D)
+    <span class="badge bg-primary rounded-pill" id="envios"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Total
+    <span class="badge bg-primary rounded-pill" id="total"></span>
+  </li>
+</ul>
+<br>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal">
+    Agregar Metodo de pago
+  </button>
+
+   `
+    ;
   }
   contenido.innerHTML = htmlContentToAppend;
 }
@@ -45,19 +70,22 @@ function subTotal(precio) {
   return (document.getElementById("resultado").innerHTML = resultado);
 }
 
+const envio = document.getElementById("envios")
 
-// entrega 6
-const premium = document.getElementById("envpremium");
-const express = document.getElementById("envexpress");
-const standard = document.getElementById("envstandard");
+  function calcular_envio() {
+    let subtotal = Number(document.getElementById("resultado").textContent)
+    let indice = opciones.selectedIndex
+    if (indice == 1)
+      envio.innerHTML = subtotal * 0.15
+    else if (indice == 2)
+      envio.innerHTML = subtotal * 0.07
+    else if (indice == 3)
+      envio.innerHTML = subtotal * 0.05
+  }
 
-
-function checkedd(precio) {
-  let cant = document.getElementById("cant").value;
-  final  = precio * cant;
-  return (document.getElementById("hola").innerHTML = final);
-
-}
-
- let aca = document.getElementById("aca");
-
+  const opciones = document.getElementById("selector")
+  
+  opciones.addEventListener("click", () => {
+    calcular_envio();
+    subTotal();
+  })
