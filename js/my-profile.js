ee6f12a889297e2769170a3be759b2ffced9a995
juansanpedro <juanignacio50@hotmail.com>
2022-11-12 21:52:29 -0300

    entrega 7 completada

---
+++ js/my-profile.js
@@ -0,0 +1,37 @@
+const email = document.getElementById("email")
+const nombre = document.getElementById("nombre")
+const segundo_nombre = document.getElementById("segundo_nombre")
+const apellido = document.getElementById("apellido")
+const segundo_apellido = document.getElementById("segundo_apellido")
+const telefono = document.getElementById("telefono")
+
+email.value=localStorage.getItem("usuario");
+nombre.value=localStorage.getItem("nombre")
+segundo_nombre.value=localStorage.getItem("segundo_nombre")
+apellido.value=localStorage.getItem("apellido")
+segundo_apellido.value=localStorage.getItem("segundo_apellido")
+telefono.value=localStorage.getItem("telefono")
+
+
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    forms.forEach(form => {
+      form.addEventListener('submit', event => {
+        event.preventDefault()
+        localStorage.setItem("nombre", nombre.value)
+        localStorage.setItem("segundo_nombre", segundo_nombre.value)
+        localStorage.setItem("apellido", apellido.value)
+        localStorage.setItem("segundo_apellido", segundo_apellido.value)
+        localStorage.setItem("telefono", telefono.value)
+
+        if (!form.checkValidity()) {
+          event.stopPropagation()
+          
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
